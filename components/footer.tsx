9cedd8d479cd489d7ed69f607d7f87c135e8d65e
paulaibfe <paulaibfe@gmail.com>
2025-07-17 18:39:43 +0200

    cyberdev se queda

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,9 @@ export function Footer() {
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
-            
+            <p className="text-sm text-muted-foreground">
+              &copy; {new Date().getFullYear()} CYBER_DEV. All rights reserved.
+            </p>
           </div>
           <div className="flex space-x-4">
             <Link href="http://linkedin.com/in/paula-ibor-4278761ba" className="text-muted-foreground hover:text-primary transition-colors">
